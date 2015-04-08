0b1e79f86a87a75d4563829560e815d86143dbe1
AdrianoSilva857 <adriano.silva857@gmail.com>
2015-04-07 23:37:32 -0300

    Ticket #23 - Ajustes no menu_base.html e criação do notão novo

--- Projeto Web/ProjetoApp/View/home.php
+++ Projeto Web/ProjetoApp/View/home.php
@@ -32,33 +32,7 @@
 </div>
 
 <div class="corpo">
-    <h1>Welcome to my home page!</h1>
-    <p>Some text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p><p>Some more text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p>
-    <p>Some more text.</p>
-
-
-
+    <h1>Seja bem vindo!</h1>
 </div>
 
 </body>
